de214045bcd72ae5e5a3cf05eba6aa7347ec25ae
SahdevPrajapati18 <sahdevprajapati27@gmail.com>
2025-10-03 11:57:13 +0530

    use memo use for preserving the value

--- ReactClass/usememo/src/App.jsx
+++ ReactClass/usememo/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import {useState} from 'react';
+import Child from './Child'
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
   return (
+    <>
     <div>
-      
+      <p>Value of Counter1: {count}</p>
+      <button onClick={()=> setCount(count+1)}>Increase</button>
     </div>
+    <br />
+    <br />
+    <div>
+      <p>Value of Counter2: {count2}</p>
+      <button onClick={()=> setCount2(count2 +1)}>
+        Increase2
+      </button>
+    </div>
+
+    <br />
+    <br />
+
+    <Child count2 = {count2}></Child>
+    </>
   )
 }
 
